bd7ba5801cdf18f6f8935622f8947d5faa292975
Michael Karbowiak <michael@karbowiak.dk>
2024-08-18 14:31:10 +0200

    Make the logger ignore the same channels the AI ignores

--- onMessage/Logger.js
+++ onMessage/Logger.js
@@ -1,5 +1,18 @@
 import { logMessageToTerminal } from '../helper.js';
+import { Config } from '../config.js';
 
 export default async function Logger(client, message) {
+    // Ignore certain channel_ids
+    let ignoredChannelIds = Config.ignoredChannelIds || [];
+    if (ignoredChannelIds.includes(message.channel.id)) {
+        return;
+    }
+
+    // Ignore certain guild_ids
+    let ignoredGuildIds = Config.ignoredGuildIds || [];
+    if (ignoredGuildIds.includes(message.guild.id)) {
+        return;
+    }
+
     logMessageToTerminal(message);
 }
